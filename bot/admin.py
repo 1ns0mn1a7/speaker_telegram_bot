19cbc96d8c8182751047ee2689262e83bcb387ca
Dmitriy877 <dmitriybukin05@gmail.com>
2025-05-20 22:31:23 +0300

    Create django and first database

---
+++ bot/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Participant, Place, Event, Question, Donat
+
+admin.site.register(Participant)
+admin.site.register(Place)
+admin.site.register(Event)
+admin.site.register(Question)
+admin.site.register(Donat)
+
+
+
+# Register your models here.
