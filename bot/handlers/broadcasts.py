c35cf24f663cd156c2e82209a98e0608c12c64fc
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-05-23 22:59:23 +0300

    Update broadcasts.py and add counter

--- bot/handlers/broadcasts.py
+++ bot/handlers/broadcasts.py
@@ -4,9 +4,12 @@ from bot.handlers.menu import send_main_menu
 
 
 def start_broadcast(user_id, chat_id, context):
-    p = Participant.objects.filter(tg_id=user_id, organizer=True).first()
-    if not p:
-        context.bot.send_message(chat_id=chat_id, text="Это доступно только организаторам.")
+    organizer = Participant.objects.filter(tg_id=user_id, organizer=True).first()
+    if not organizer:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text="Это доступно только организаторам."
+        )
         return False
     context.bot_data["user_states"][user_id] = "awaiting_broadcast_text"
     context.bot.send_message(chat_id=chat_id, text="Введите текст рассылки:")
@@ -16,15 +19,32 @@ def start_broadcast(user_id, chat_id, context):
 def handle_broadcast(text, user_id, chat_id, context):
     context.bot_data["user_states"][user_id] = None
     header = "⚡ Новость от организаторов:"
-    recipients = Participant.objects.filter(subscriber=True, tg_id__isnull=False)
+    recipients = Participant.objects.filter(
+        subscriber=True,
+        tg_id__isnull=False
+    )
 
+    success_count = 0
     for sub in recipients:
         try:
-            context.bot.send_message(chat_id=sub.tg_id, text=f"<b>{header}</b>\n\n{text}", parse_mode="HTML")
+            context.bot.send_message(
+                chat_id=sub.tg_id,
+                text=f"<b>{header}</b>\n\n{text}",
+                parse_mode="HTML")
+            success_count += 1
         except Exception:
-            pass
+            continue
 
-    msg = Message.objects.create(header=header, message=text, creation_date=now(), send_status=True)
+    msg = Message.objects.create(
+        header=header,
+        message=text,
+        creation_date=now(),
+        send_status=True
+    )
     msg.recipent.set(recipients)
-    context.bot.send_message(chat_id=chat_id, text="Рассылка отправлена ✅")
+
+    context.bot.send_message(
+        chat_id=chat_id,
+        text=f"Рассылка отправлена ✅ ({success_count}/{recipients.count()} участников)"
+    )
     send_main_menu(chat_id, context)
